1f79e6bdd90bb0bfdc2581e7feb6d76af339977b
sakethc4 <saketh.c4@gmail.com>
2024-11-07 17:31:41 -0500

    initial

---
+++ USACO/Shell_Game_Bronze.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+using std::vector;
+
+    int main() {
+        freopen("shell.in", "r", stdin);
+
+        int n;
+        scanf("%d", &n);
+        vector<int> shell_at_pos(3);
+        for (int i = 0; i < 3; i ++) { shell_at_pos[i] = i; }
+        // Count guesses 
+        vector<int> counter(3);
+        for (int i = 0; i < n; i++) {
+            int a, b, g;
+            scanf("%d %d %d", &a, &b, &g);
+            // Since we zero index we got to decrement. 
+            a --, b--, g--;
+            std::swap(shell_at_pos[a], shell_at_pos[b])
+            counter[shell_at_pos[g]]++;
+        }
+        freopen("shell.out", "w", stdout);
+        printf("%d/n", std::max({counter[0], counter[1], counter[2]}));
+    }
